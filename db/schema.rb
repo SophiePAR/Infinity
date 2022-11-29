292a708fb44209ad0b8cb44efc2b40a6baa20008
Alexei Cazacu <alexvspishka@gmail.com>
2022-11-29 16:40:38 +0100

    nav bar + homme page

--- db/schema.rb
+++ db/schema.rb
@@ -78,6 +78,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_115140) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "first_name", default: "", null: false
+    t.string "last_name", default: "", null: false
+    t.string "phone", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
