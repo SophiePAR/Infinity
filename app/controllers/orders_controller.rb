9327600c17f4acb3d4d7a5813a0b57cb0e306ec6
sophie Parisot <sophie.parisot.93@gmail.com>
2022-11-30 13:06:06 +0100

    ordershowlist

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,36 @@
 class OrdersController < ApplicationController
+
   def show
     @order = Order.find(params[:id])
+    # @order_items = OrderItem.all
   end
 
   def new
-
   end
 
   def create
+    # @user = User.find(params[:user_id])
+    # @tombstone = Tombstone.find(params[:tombstone_id])
+    # @order = Order.new(order_params)
+    # @order.tombstone_id = @tombstone.id
+    # @order.user_id = @user.id
+    # if @order.save
+    #   render action: OrderItem.new
+    # else
+    #   render :new, status: :unprocessable_entity
+    # end
   end
 
   def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+
+    redirect_to order_path(@order)
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:review, :rating, :date, :progress)
   end
 end
--- app/controllers/prestations_controller.rb
+++ app/controllers/prestations_controller.rb
@@ -1,4 +1,18 @@
 class PrestationsController < ApplicationController
   def create
-  end
-end
+    if @prestation.title == 'Autres'
+    end
+  #   @prestation = Prestation.new(order_params)
+  #   if @prestation.save
+  #     redirect_to order_item_path(@prestation)
+  #   else
+  #     render new, status: :unprocessable_entity
+  #   end
+  # end
+
+  # private
+
+  # def prestation_params
+  #   params.require(:prestation).permit(:title, :price, :content)
+  # end
+ends
