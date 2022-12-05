65e2b98fb0a2d568e736c0a203558f9d581c299a
sophie Parisot <sophie.parisot.93@gmail.com>
2022-12-05 17:54:23 +0100

    map

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -46,19 +46,53 @@ export default class extends Controller {
   }
 
   clickmap() {
-    this.map.on('click', (e) => {
 
-    // this.longTarget.value = parseFloat(e.lngLat.lng)
-    // this.latTarget.value = parseFloat(e.lngLat.lat)
-    this.longTarget.value = e.lngLat.lng
-    console.log(e.lngLat.lng)
-    this.latTarget.value = e.lngLat.lat
+    // this.map = new mapboxgl.Marker()({
+    // container: "map",
+    // });
+    // this.marker = new mapboxgl.Marker();
+    // this.map.on('click', this.add_marker.bind(this));
 
-        new mapboxgl.Marker()
-        .setLngLat([ e.lngLat.lng , e.lngLat.lat ])
-        .addTo(this.map)
-    })
+    // function add_marker  (event) {
+    //   var coordinates = event.lngLat;
+    //   console.log('Lng:', coordinates.lng, 'Lat:', coordinates.lat);
+    //   this.marker.setLngLat(coordinates).addTo(this.map);
+    // }
 
+    // this.map.on('click', (e) => {
+
+    // // this.longTarget.value = parseFloat(e.lngLat.lng)
+    // // this.latTarget.value = parseFloat(e.lngLat.lat)
+    // this.longTarget.value = e.lngLat.lng
+    // console.log(e.lngLat.lng)
+    // this.latTarget.value = e.lngLat.lat
+
+    //     new mapboxgl.Marker()
+    //     .setLngLat([ e.lngLat.lng , e.lngLat.lat ])
+    //     .addTo(this.map)
+    // })
+    var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-79.4512, 43.6568],
+    zoom: 13
+    });
+    this.map = new mapboxgl.Map({
+      container: "map",
+      style: "<mapbox://styles/mapbox/streets-v11",
+    });
+    this.marker = new mapboxgl.Marker();
+    this.map.on('click', this.add_marker.bind(this));
+    function add_marker  (event) {
+      var coordinates = event.lngLat;
+      console.log('Lng:', coordinates.lng, 'Lat:', coordinates.lat);
+      this.marker.setLngLat(coordinates).addTo(this.map);
+    }
+    var geocoder = new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    mapboxgl: mapboxgl
+    });
+    document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
 
   }
 }
