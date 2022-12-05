aa8037aad181ee7d0673550b9d6fbc461deee6ae
franci-lobbie lalane <francilobbie.lalane@gmail.com>
2022-12-05 18:32:57 +0100

    tombstone

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,20 @@
 class MessagesController < ApplicationController
+
   def create
+    @tombstone = Tombstone.find(params[:tombstone_id])
+    @message = Message.new(message_params)
+    @message.tombstone = @tombstone
+    @message.user = current_user
+    if @message.save
+      redirect_to tombstone_path(@tombstone)
+    else
+      render "tombstobne/show", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
   end
 end
