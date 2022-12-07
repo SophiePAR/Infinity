7a24b29a93fdea1803fdd015cf11469ce547e478
franci-lobbie lalane <francilobbie.lalane@gmail.com>
2022-12-07 10:33:49 +0100

    not ok

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -10,8 +10,8 @@ application.register("flatpickr", FlatpickrController)
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
-import MapController from "./map_controller"
-application.register("map", MapController)
+// import MapController from "./map_controller"
+// application.register("map", MapController)
 
 import TombstoneSubscriptionController from "./tombstone_subscription_controller"
 application.register("tombstone-subscription", TombstoneSubscriptionController)
